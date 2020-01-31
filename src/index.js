48cffb9db3d1258d303a015e948110f6ac8b773f
noomdalv <vladlunasan@gmail.com>
2020-01-31 16:20:57 -0500

    added temperature toggle functionality 404 error handling

--- src/index.js
+++ src/index.js
@@ -1,45 +1,78 @@
+// HTML Variables //
+
 const cityName = document.getElementById('cityInput');
 const searchBtn = document.getElementById('searchBtn');
+const errorMsg = document.getElementById('errorMsg');
+const displayInfo = document.getElementById('displayInfo');
 const cityInfo = document.getElementById('cityInfo');
 const tempInfo = document.getElementById('tempInfo');
-const switchTemp = document.getElementById('switchTempBtn');
+const switchTempBtn = document.getElementById('switchTempBtn');
 const statusInfo = document.getElementById('statusInfo');
 const statusImg = document.getElementById('statusImg');
 const windInfo = document.getElementById('windInfo');
-let temp;
+let temp, tempUnit;
 
 const getFahrenheit = (tempCelsius) => {
 	let tempFahrenheit = (tempCelsius / (5/9)) + 32;
-	return (tempFahrenheit.toFixed(2) + " °F");
+	temp = tempFahrenheit.toFixed(2);
+	tempUnit = "F";
+	tempInfo.innerHTML = (temp + " °F");
+	switchTempBtn.innerHTML = "Switch to °C";
 }
 
 const getCelsius = (tempFahrenheit) => {
 	let tempCelsius = (tempFahrenheit - 32) * (5/9);
-	return (tempCelsius.toFixed(2) + " °C");
+	temp = tempCelsius.toFixed(2);
+	tempUnit = "C"
+	tempInfo.innerHTML = (temp + " °C");
+	switchTempBtn.innerHTML = "Switch to °F";
 }
 
-// getFahrenheit(5.15);
-// getCelsius(41.27);
+const displayWeatherData = (data) => {
+	if (data.cod == "200") {
+		errorMsg.style.visibility = "hidden";
+		displayInfo.style.visibility = "visible";
+		cityInfo.innerHTML = data.city.name;
+		windInfo.innerHTML = ((data.list[0].wind.speed) + " m/s");
+		statusInfo.innerHTML = data.list[0].weather[0].description;
+		statusImg.src = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+		tempInfo.innerHTML = data.list[0].main.temp + " °C";
+		temp = data.list[0].main.temp;
+		tempUnit = "C";
+	} else if (data.cod == "404") {
+		errorMsg.style.visibility = "visible";		
+		displayInfo.style.visibility = "hidden";
+	}
+}
 
-const getWeather = async (city, temp) => {
-	const response = await fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=c414b36c42d82ca02be257a1553e41c6&units=metric&q=" + cityName.value);
-	const weatherData = await response.json();
-	console.log(weatherData);
-	cityInfo.innerHTML = weatherData.city.name;
-	windInfo.innerHTML = ((weatherData.list[0].wind.speed) + " m/s");
-	statusInfo.innerHTML = weatherData.list[0].weather[0].description;
-	statusImg.src = `http://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`;
-	temp = weatherData.list[0].main.temp;
-	tempInfo.innerHTML = temp + " °C";
+const getWeather = async (city) => {
+		const response = await fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=c414b36c42d82ca02be257a1553e41c6&units=metric&q=" + cityName.value);
+		const weatherData = await response.json();
+		console.log(weatherData);
+		await displayWeatherData(weatherData);
 }
 
 searchBtn.addEventListener("click", () => {
-	getWeather(cityName);
+	if (cityName.value.length > 0) {
+		getWeather(cityName);
+	}
 })
 
 cityName.addEventListener("keydown", () => {
 	if (event.key === "Enter") {
     event.preventDefault();
 		searchBtn.click();
+	}
+})
+
+switchTempBtn.addEventListener("click", () => {
+	console.log("temp = " + temp);
+	console.log("tempUnit = " + tempUnit);
+	if (tempUnit === "C") {
+		console.log("a");
+		getFahrenheit(temp);
+	} else if (tempUnit === "F") {
+		console.log("b");
+		getCelsius(temp);
 	}
 })
