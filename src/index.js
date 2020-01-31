91111217119326b1323314924ff67dbc1db6729f
noomdalv <vladlunasan@gmail.com>
2020-01-31 13:01:49 -0500

    added functionality for weather status and styling for displaying info

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-console.log("hi")
-
 const cityName = document.getElementById('cityInput');
 const searchBtn = document.getElementById('searchBtn');
 const cityInfo = document.getElementById('cityInfo');
 const tempInfo = document.getElementById('tempInfo');
+const switchTemp = document.getElementById('switchTempBtn');
+const statusInfo = document.getElementById('statusInfo');
+const statusImg = document.getElementById('statusImg');
+const windInfo = document.getElementById('windInfo');
+let temp;
 
 const getFahrenheit = (tempCelsius) => {
 	let tempFahrenheit = (tempCelsius / (5/9)) + 32;
-	console.log(tempFahrenheit.toFixed(2) + " °F");
+	return (tempFahrenheit.toFixed(2) + " °F");
 }
 
 const getCelsius = (tempFahrenheit) => {
 	let tempCelsius = (tempFahrenheit - 32) * (5/9);
-	console.log(tempCelsius.toFixed(2) + " °C");
+	return (tempCelsius.toFixed(2) + " °C");
 }
 
-getFahrenheit(5.15);
-getCelsius(41.27);
+// getFahrenheit(5.15);
+// getCelsius(41.27);
 
-const getWeather = async (city) => {
+const getWeather = async (city, temp) => {
 	const response = await fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=c414b36c42d82ca02be257a1553e41c6&units=metric&q=" + cityName.value);
 	const weatherData = await response.json();
 	console.log(weatherData);
 	cityInfo.innerHTML = weatherData.city.name;
-	tempCelsius = weatherData.list[0].main.temp;
-	tempInfo.innerHTML = tempCelsius + " °C";
+	windInfo.innerHTML = ((weatherData.list[0].wind.speed) + " m/s");
+	statusInfo.innerHTML = weatherData.list[0].weather[0].description;
+	statusImg.src = `http://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`;
+	temp = weatherData.list[0].main.temp;
+	tempInfo.innerHTML = temp + " °C";
 }
 
 searchBtn.addEventListener("click", () => {
@@ -34,7 +40,6 @@ searchBtn.addEventListener("click", () => {
 cityName.addEventListener("keydown", () => {
 	if (event.key === "Enter") {
     event.preventDefault();
-		console.log("zzzzzz")
 		searchBtn.click();
 	}
 })
